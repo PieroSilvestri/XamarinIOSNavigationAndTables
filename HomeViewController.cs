1d1425134a82c1cf9728d767474f661eb67b6bd6
munsra <piero.silvestri93@gmail.com>
2017-05-26 10:18:42 +0200

    Main Storyboard build

---
+++ HomeViewController.cs
@@ -0,0 +1,28 @@
+using Foundation;
+using System;
+using UIKit;
+using System.Diagnostics;
+
+namespace XamarinIOSNavigationAndTables
+{
+    public partial class HomeViewController : UIViewController
+    {
+        public HomeViewController (IntPtr handle) : base (handle)
+        {
+        }
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+
+			Title = "Home";
+
+			TableButton.TouchUpInside += GoToTablePage;
+		}
+
+		private void GoToTablePage(object sender, EventArgs e)
+		{
+			Debug.WriteLine("GoToTablePage");
+		}
+    }
+}
